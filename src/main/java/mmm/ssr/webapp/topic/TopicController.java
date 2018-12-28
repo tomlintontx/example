20ad2a7315a571a8d864464b831d6c5ceb34b2e5
tomlintontx <tomlintontx@gmail.com>
2018-12-28 15:49:25 -0700

    added the rest of the rest service

--- src/main/java/mmm/ssr/webapp/topic/TopicController.java
+++ src/main/java/mmm/ssr/webapp/topic/TopicController.java
@@ -1,10 +1,12 @@
 package mmm.ssr.webapp.topic;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,5 +19,24 @@ public class TopicController {
 	public List<Topic> getAllTopics() {
 		return topicService.getAllTopics();
 	}
+
+	@RequestMapping(value="/topics/{id}", method=RequestMethod.GET)
+	public Topic getTopic(@PathVariable String id) {
+		return topicService.getTopic(id);
+	}
 	
+	@RequestMapping(value="/topics", method=RequestMethod.POST)
+	public void addTopic(@RequestBody Topic topic) {
+		topicService.addTopic(topic);
+	}
+	
+	@RequestMapping(value="/topics/{id}", method=RequestMethod.PUT)
+	public void updateTopic(@RequestBody Topic topic, @PathVariable String id) {
+		topicService.updateTopic(id, topic);
+	}
+
+	@RequestMapping(value="/topics/{id}", method=RequestMethod.DELETE)
+	public void deleteTopic(@PathVariable String id) {
+		topicService.deleteTopic(id);
+	}
 }
--- src/main/java/mmm/ssr/webapp/topic/TopicService.java
+++ src/main/java/mmm/ssr/webapp/topic/TopicService.java
@@ -1,5 +1,6 @@
 package mmm.ssr.webapp.topic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,13 +9,35 @@ import org.springframework.stereotype.Service;
 @Service
 public class TopicService {
 	
-	private List<Topic> topics = Arrays.asList(
+	private List<Topic> topics = new ArrayList<>(Arrays.asList(
 			new Topic("spring","spring framework", "this is my description"),
 			new Topic("tom","tom linton", "he is awesome"),
 			new Topic("ssr","self service reporting", "sweeeeeeeeeeeet")
-			);
+			));
 	
 	public List<Topic> getAllTopics() {
 		return topics;
+	}
+
+	public Topic getTopic(String id) {
+		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+
+	public void addTopic(Topic topic) {
+		topics.add(topic);
+	}
+
+	public void updateTopic (String id, Topic topic) {
+		for (int i=0; i < topics.size(); i++) {
+			Topic t = topics.get(i);
+			if (t.getId().equals(id)) {
+				topics.set(i, topic);
+				return;
+			}
+		}
+	}
+
+	public void deleteTopic(String id) {
+		topics.removeIf(t -> t.getId().equals(id));
 	}
 }
